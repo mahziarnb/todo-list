b43e82289a85dec6c4fd4301739e2e787fcce81d
mahziar <mahziarnazaribavarsad@gmail.com>
2026-08-07 21:27:27 +0800

    refactor: change task addition logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 'use strict'
 const tasksList = document.getElementById('tasks-list')
+
+function addItemFunc(taskText) {
+
 // -- create list item start --
 const li = document.createElement('li')
 li.classList.add('tasks-item')
+tasksList.append(li)
 
 const input = document.createElement('input')
 input.classList.add('item-input')
+input.value = taskText
 li.append(input)
 
 const div = document.createElement('div')
@@ -28,3 +33,29 @@ const removeIcon = document.createElement('i')
 removeIcon.classList.add('fa-solid' , 'fa-trash')
 removeBtn.append(removeIcon)
 // -- create list item end --
+
+}
+
+
+// -- add process start --
+const addBtn = document.getElementById('addBtn')
+const addInput = document.getElementById('add-task-input')
+const itemInputs = document.querySelectorAll('.item-input')
+
+addBtn.addEventListener('click' , () => {
+    if(addInput.value.trim() === '') {
+        alert(`the input is empty`)
+        return
+    }
+    addItemFunc(addInput.value)
+    addInput.value = ''
+    addInput.focus()
+})
+
+// access key
+addInput.addEventListener('keydown' , (event) => {
+    if(event.key === 'Enter'){
+        addBtn.click()
+    }
+})
+// -- add process end --
